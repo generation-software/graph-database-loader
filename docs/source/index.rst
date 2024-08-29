7ee6610766f070a37964ffbc729777ec16669fa1
Jack <jack20220723@gmail.com>
2024-08-29 15:23:04 +0800

    Support grainer vocabulary upload

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -5,6 +5,90 @@ Wilhelm Graph Database Python SDK
 .. contents:: Table of Contents
     :depth: 2
 
+Install
+=======
+
+To install the SDK, simply run
+
+```console
+pip install wilhelm-graphdb-python
+```
+
+Upload Vocabulary CSV into Neo4J
+================================
+
+# Make ready a Neo4J database instance. A free one can be obtained at https://console.neo4j.io
+# Set the following environment variables
+
+  - `NEO4J_URI`: the connection URL of the database, such as "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+  - `NEO4J_USERNAME`: the username for connecting the database
+  - `NEO4J_PASSWORD`: the user's password for the connection
+
+  where all of them are available when database is provisioned on https://console.neo4j.io
+
+# Have a vocabulary file of the following YAML format ready at __german.yaml__ (in this example, we are loading a
+  German vocabulary):
+
+  ```yaml
+  vocabulary:
+    - term: "null"
+      definition: 0
+    - term: Guten Tag
+      definition: Good day
+    - term: Hallo
+      definition: Hello
+    - term: Ich heiße ...
+      definition: I am called ...
+    - term: Mein Name ist ...
+      definition: My name is ...
+    - term: bitte
+      definition: please
+  ```
+
+# Load vocabulary into Neo4J database:
+
+  .. code-block:: python
+
+     from wilhelm_graphdb_python.neo4j_loader import load_into_database
+
+     load_into_database("german.yaml", "German")
+
+.. tip::Transform CSV into YAML
+   The reason we use YAML is because it supports multi-line field value, a common case for vocabulary definitions. But
+   in case the vocabulary is in CSV, the following Python script can be used to transform it into compatible YAML used
+   in this library:
+
+   .. code-block:: python
+      import csv
+
+      with open("/file/path/to/vocabulary.csv") as f:
+          reader = csv.reader(f, delimiter=',')
+
+          with open("/file/path/to/vocabulary.yaml", mode='w') as outfile:
+              outfile.write("vocabulary:\n")
+
+              for row in reader:
+                  outfile.write("  - term: {}\n    definition: {}\n".format(row[0], row[1]))
+
+   The following words must be quoted in YAML file in order to prevent them from being interpolated by YAML syntax
+
+   - null
+   - no
+
+If this is an experiment and we would like to empty database,
+`simply run <https://stackoverflow.com/a/60933970/14312712>`_::
+
+    match (a) -[r] -> () delete a, r;
+    match (a) delete a;
+
+API Documentation
+=================
+
+.. automodule:: wilhelm_graphdb_python.quizlet
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
 Development
 ===========
 
@@ -20,11 +104,3 @@ the dependencies for development::
 
     python setup.py egg_info
     pip install -r wilhelm_graphdb_python.egg-info/requires.txt
-
-API Documentation
-=================
-
-.. automodule:: wilhelm_graphdb_python.quizlet
-   :members:
-   :undoc-members:
-   :show-inheritance:
--- wilhelm_graphdb_python/neo4j_loader.py
+++ wilhelm_graphdb_python/neo4j_loader.py
@@ -13,18 +13,86 @@
 # limitations under the License.
 import os
 
+import yaml
 from neo4j import GraphDatabase
 
 URI = os.environ["NEO4J_URI"]
 AUTH = (os.environ["NEO4J_USERNAME"], os.environ["NEO4J_PASSWORD"])
 
 
-def load_into_database(vocabulary: list[tuple[str, str]]):
+def get_vocabulary(yaml_path: str) -> list[tuple[str, str]]:
+    with open(yaml_path, "r") as f:
+        data = yaml.safe_load(f)
+
+    return [(vocabulary["term"], vocabulary["definition"]) for vocabulary in data["vocabulary"]]
+
+
+def load_into_database(yaml_path: str, language: str):
+    """
+    Upload a vocabulary file to Neo4j Database.
+
+    The YAML format has to be the following::
+
+    vocabulary:
+      - term: 你好
+        definition: hello
+      - term: 一
+        definition: 1
+      - term: 二
+        definition: 2
+      ...
+
+    The function handles MANY-TO-MANY relationships. i.e. multiple terms with the same definition and multiple
+    definitions with the same term. Each unique term and definition are separate nodes in graph. The MANY-TO-MANY
+    relationships show up naturally as multi incident/outgoing edges of the nodes.
+
+    :param yaml_path:  The absolute or relative path (to the invoking script) to the YAML file that containts the
+                       vocabulary structure mentioned above.
+    :param language:  The language of the vocabulary. This value will show up as the "language" property of the `Term`
+                      node in the Graph database.
+    """
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
 
+    vocabulary = get_vocabulary(yaml_path)
+    unique_terms = set(term for term, definition in vocabulary)
+    unique_definitions = set(definition for term, definition in vocabulary)
+
     with driver.session() as session:
         session.write_transaction(
-            lambda tx, nodes: [tx.run("CREATE (:Vocabulary $props)", props=node) for node in nodes],
+            lambda tx, terms: [
+                tx.run(
+                    "CREATE (term:Term {name: $term, language: $language})",
+                    term=term,
+                    language=language,
+                ) for term in terms
+            ],
+            unique_terms
+        )
+        session.write_transaction(
+            lambda tx, definitions: [
+                tx.run(
+                    "CREATE (definition:Definition {name: $definition})",
+                    definition=definition,
+                ) for definition in definitions
+            ],
+            unique_definitions
+        )
+
+        session.write_transaction(
+            lambda tx, nodes: [
+                tx.run(
+                    """
+                    MATCH
+                        (term:Term WHERE term.name = $term AND term.language = $language),
+                        (definition:Definition WHERE definition.name = $definition)
+                    CREATE
+                        (term)-[:DEFINITION]->(definition)
+                    """,
+                    term=node["name"],
+                    language=language,
+                    definition=node["definition"],
+                ) for node in nodes
+            ],
             [{"name": term, "definition": definition} for term, definition in vocabulary]
         )
